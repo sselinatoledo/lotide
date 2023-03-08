9714c4fb8eb912faf6452f3c1355e9c43278095a
sselinatoledo <sselinatoledo@gmail.com>
2023-03-08 21:44:29 +0000

    Interpolate String and Non-Identical

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-  } else console.log("🛑🛑🛑 Assertion Failed:" + actual + " !== " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp"); //non-identical strings
+assertEqual(1, 1); //identical numbers
+assertEqual("JavaScript", "JavaScript"); //identical strings
+assertEqual(2, 1); //non-identical numbers
