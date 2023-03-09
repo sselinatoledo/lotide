ec80dc97786ac40ca3f28ced07b1b6b9472e2c9f
sselinatoledo <sselinatoledo@gmail.com>
2023-03-09 04:30:47 +0000

    assertArraysEqual added

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+console.log(assertArraysEqual([], []));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
