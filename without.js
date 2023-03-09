207fb9ec8ba0d55d286a82a296405753a908df6c
sselinatoledo <sselinatoledo@gmail.com>
2023-03-09 04:33:15 +0000

    without.js added

---
+++ without.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]);
+  } else {
+    console.log(🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (const item of source) {
+    if (!itemsToRemove.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
