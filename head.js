aab30fdedcfda15e35275168b7810f9091e6d350
sselinatoledo <sselinatoledo@gmail.com>
2023-03-24 18:35:15 +0000

    update head.js code

--- head.js
+++ head.js
@@ -1,16 +1,17 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  
+  }
 };
 
-const head = function(zero) {
-  return zero[0];
+const head = function(actual) {
+  return actual[0]
 }
-  
+
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 5)
+assertEqual(head(["Hello", "Lighthosue", "Labs"]), "Hello");
