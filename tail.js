2b9b4914c0cc71d971268f280752b6d38e9dbd8c
sselinatoledo <sselinatoledo@gmail.com>
2023-03-24 23:19:14 +0000

    updated tail.js code

--- tail.js
+++ tail.js
@@ -1,13 +1,22 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  
+  }
 };
-const tail = function(arr) {
-  return arr.slice(0);
+
+const tail = function(actual) {
+  return actual.slice(1);
 };
+
+
+// TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-// TEST CODE
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
