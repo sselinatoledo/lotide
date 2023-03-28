1380528545d7f5ded345e04488d24d0e1b5d8859
sselinatoledo <sselinatoledo@gmail.com>
2023-03-28 05:07:38 +0000

    updated codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,42 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑Assertion Failed: ${actual} !=== ${expected}`);
-};
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
-  return true;
 };
 
-console.log(eqArrays([], []));
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])) // => false       // add another tester
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+// use assertEqual function => test cases for various scenarios 
+
+// TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true) // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => should FAIL
