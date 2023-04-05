c0c70a801fcc3bb28bf91dc950dca6b334830c46
sselinatoledo <sselinatoledo@gmail.com>
2023-04-05 03:09:04 +0000

    update pseudocode in findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,20 +5,26 @@ const assertEqual = function(actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
-
+// INSTRUCTIONS:
 // should scan the object and return the first key which contains the given value. 
 // if no key with that given value is found, then return undefined
 
+// CODE:
 // implement the function findKeyByValue. which takes in an object and a value.
 const findKeyByValue = function(object, value) {
+  // for each key in the object. this is the loop in order to access the properties of an object.
   for (let key in object) {
+    // check if the value of the current key = to the given value.
     if (object[key] === value) {
+      //if it is, return the key.
       return key
     }
   }
+  // if the loop completes, and without finding a match, return undefined
   return undefined
 }
 
+// we need this in order to have the output and function findKeyByValue to work.
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
