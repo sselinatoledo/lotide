2bb208e97f42aa3f3aa551f7b38189ecedd32ff6
sselinatoledo <sselinatoledo@gmail.com>
2023-04-17 10:06:31 +0000

    added recursive test codes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,27 +1,25 @@
 // test assertEqual.js
 const assertEqual = require('../assertEqual');
 
-
 // test eqArrays.js
 const eqArrays = require('../eqArrays');
 
-// TEST CODE
+// TEST CODE 1
 console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false, add another tester
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-// use assertEqual function => test cases for various scenarios
-
-// TEST CODE
+// TEST CODE 2
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-// TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => should FAIL
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+
+// TEST CODE 3 
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log(eqArrays([[2, 3], [4]], [2, 3], [4, 5])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
