297b78fbb516551dedc907de0299e559a6660069
sselinatoledo <sselinatoledo@gmail.com>
2023-04-17 09:37:34 +0000

    separate test code from the function

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -5,8 +5,8 @@ const eqObjects = require('../eqObjects');
 const assertEqual = require('../assertEqual');
 
 
-// PRIMITIVE AS VALUES (TESTS)
-// created objects to be compared
+// // PRIMITIVE AS VALUES (TESTS)
+// // created objects to be compared
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
 console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
@@ -18,7 +18,7 @@ assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // assertion pass
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // assertion passed!
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), true); // assertion failed!
 
-// ARRAYS AS VALUES (TESTS)
+// // ARRAYS AS VALUES (TESTS)
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)); // => true
@@ -32,3 +32,7 @@ assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), t
 
 // OBJECTS AS VALUES (DISCUSSION)
 // we'll be coming back to it soon enough.
+// STRETCH: recursion. we are back.
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
