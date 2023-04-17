4d980575178071a5624af72f972c6b2b6bf2bbaa
sselinatoledo <sselinatoledo@gmail.com>
2023-04-17 09:34:19 +0000

    recursive eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,33 +1,40 @@
 // require the eqArrays function.
 const eqArrays = require('./eqArrays');
 
-// Define a function called eqObjects. that takes in two objects as parameters.
+// FUNCTION
 const eqObjects = function(object1, object2) {
-  // Get the keys of the 2 objects. Object.keys() method. STEP 3 PRIMITIVE AS VALUES (IMPLEMENTATION).
-  let key1 = Object.keys(object1);
-  let key2 = Object.keys(object2);
-  // check if the length of the index from 2 objects are not equal.
-  if (key1.length !== key2.length) {
-    // return false
+  
+  // check if both objects have the same number of keys
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
     return false;
   }
-  // next, for in loop through the properties of the object.
-  for (let key in key1) {
-    // if both objects have arrays
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      // call eqArrays function to check if they are not equal
-      if (!eqArrays(object1[key] !== object2[key])) {
-        // return false
+  // Iterate over each key in object1
+  for (let key of keys1) {
+    const value1 = object1[key]
+    const value2 = object2[key]
+    
+    // check if the value of value1 is an array.
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
         return false;
-        // need to change the if to else if so it will skip if the answer above is true(meaning 2 arrays are equal)
-        // if current index(value) from 2 objects are not equal.
-      } else if (object1[key] !== object2[key]) {
-        // return false
+      }
+    }  
+      // check if the value of value2 is an object.
+     else if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!Array.isArray(value1) && !Array.isArray(value2)) {
+      if (!eqObjects(value1, value2)) {
         return false;
       }
     }
-    // if all matches, return true
-    return true;
+    // compare the values directly.
+    } else if (value1 !== value2) {
+      return false;
+    }
   }
+  return true;
 };
 module.exports = eqObjects;
+
+
