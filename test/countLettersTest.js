0cec5a76b3152ad9186cd8d10b9dbff64cf60f90
sselinatoledo <sselinatoledo@gmail.com>
2023-04-18 05:38:14 +0000

    mocha and chai testing

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,16 +1,16 @@
 // test countLetters.js function.
-const countLetters = require('../countLetters');
+// const countLetters = require('../countLetters');
 
-// test assertEqual in countLetters function.
-const assertEqual = require('../assertEqual');
+// // test assertEqual in countLetters function.
+// const assertEqual = require('../assertEqual');
 
-// INITIAL TEST CODE
-console.log(countLetters("lighthouse in the house"));
-const result1 = countLetters("lighthouse in the house");
-assertEqual(result1["l"], 1);
-assertEqual(result1["c"], undefined);
-assertEqual(result1["i"], 2);
-assertEqual(result1["h"], undefined);
+// // INITIAL TEST CODE
+// console.log(countLetters("lighthouse in the house"));
+// const result1 = countLetters("lighthouse in the house");
+// assertEqual(result1["l"], 1);
+// assertEqual(result1["c"], undefined);
+// assertEqual(result1["i"], 2);
+// assertEqual(result1["h"], undefined);
 
 // OUTPUT
 /*
@@ -27,3 +27,22 @@ assertEqual(result1["h"], undefined);
   n: 1,
 }
 */
+
+// MOCHA AND CHAI TESTING
+const assert = require('chai').assert;
+const countLetters = require('../countLetters');
+
+describe("countLetters function", function() {
+  it("returns 1 === 1 for (countLetters('lighthouse in the house')['l'], 1)", function() {
+    assert.deepEqual(countLetters("lighthouse in the house")["l"], 1);
+  });
+  it("returns undefined === undefined for (countLetters('lighthouse in the house')['c'], undefined)", function() {
+    assert.deepEqual(countLetters("lighthouse in the house")["c"], undefined);
+  });
+  it("returns 2 === 2 for (countLetters('lighthouse in the house')['i'], 2)", function() {
+    assert.deepEqual(countLetters("lighthouse in the house")["i"], 2);
+  });
+  it("returns 1 === 1 for (countLetters('lighthouse in the house')['h'], undefined)", function() {
+    assert.deepEqual(countLetters("lighthouse in the house")["h"], 4);
+  });
+});
