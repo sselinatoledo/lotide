e8299beb2db989faa3fa0b408c17b8b5cab63498
sselinatoledo <sselinatoledo@gmail.com>
2023-04-18 06:44:51 +0000

    update code

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,7 +2,7 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 // Test Code
-assertArraysEqual([1, 2, 3], [1, 2, 3]);   // passed!
-assertArraysEqual([1, 2, 3], [3, 2, 1]);   // failed!
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);   // passed!
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);     // failed!
+assertArraysEqual([1, 2, 3], [1, 2, 3]);   // passed! all values in each index are the same.
+assertArraysEqual([1, 2, 3], [3, 2, 1]);   // failed! the value in each index are not the same.
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);   // passed! all strings.
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);     // failed! 3 is not a string.
