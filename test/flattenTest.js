627390b54efa4b2e15a1db92f497ff41725b3220
sselinatoledo <sselinatoledo@gmail.com>
2023-04-18 05:21:25 +0000

    testing mocha and chai

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,10 +1,23 @@
 // require functions to run the test code.
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const flatten = require('../flatten');
+
+// // TEST CODE 1
+// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => PASS
+
+// // TEST CODE 2
+// assertArraysEqual(flatten([6, 5, [4, 3], 2, [1]]), [6, 5, 4, 3, 2, 1]); // => PASS
+// assertArraysEqual(flatten([6, 5, [4, 3], 2, [1]]), [2, 4, 6, 8]); // => FAIL
+
+// MOCHA AND CHAI TESTING
+const assert = require('chai').assert;
 const flatten = require('../flatten');
 
-// TEST CODE 1
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => PASS
-
-// TEST CODE 2
-assertArraysEqual(flatten([6, 5, [4, 3], 2, [1]]), [6, 5, 4, 3, 2, 1]); // => PASS
-assertArraysEqual(flatten([6, 5, [4, 3], 2, [1]]), [2, 4, 6, 8]); // => FAIL
+describe("flatten function", function() {
+  it("returns 1,2,3,4,5,6 === 1,2,3,4,5,6 for (flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])", function() {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+  it("returns 6,5,4,3,2,1 === 6,5,4,3,2,1 for ([6, 5, [4, 3], 2, [1]]), [6, 5, 4, 3, 2, 1])", function() {
+    assert.deepEqual(flatten([6, 5, [4, 3], 2, [1]]), [6, 5, 4, 3, 2, 1]);
+  });
+});
