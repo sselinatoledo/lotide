1cca0cf673d29c9ec274027e363cafd5603c7a60
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 20:56:25 +0000

    remove flatten minor comments

--- flatten.js
+++ flatten.js
@@ -1,27 +1,13 @@
-// function that takes in an array that contains elements including nested array of elements and returns a flattened version of the array.
 const flatten = function(nested) {
-
-  // store the merged array.
   let newArray = [];
-
-  // loop through the array.
   for (let i of nested) {
-
-    // if the nested arrays is an array.
     if (Array.isArray(i)) {
-
-      // concatenate the array and putting it as the newArray.
       newArray = newArray.concat(flatten(i));
-
-      // otherwise, push method to push the element inside the buffer if the array is not a nested array.
     } else {
       newArray.push(i);
     }
   }
-
-  // return the newArray.
   return newArray;
 };
 
-// export flatten function.
 module.exports = flatten;
