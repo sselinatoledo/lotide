f7fc5febc5a51d637bd3c4316d1eb725feb64ace
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 20:56:39 +0000

    remove head minor comments

--- head.js
+++ head.js
@@ -1,7 +1,5 @@
-// function that takes in an array and returns the first element in the array.
 const head = function(actual) {
   return actual[0];
 };
 
-// export head function.
 module.exports = head;
