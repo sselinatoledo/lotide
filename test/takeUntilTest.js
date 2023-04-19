2cffceb88cc0a7a76087172f833a86d6f190f0a7
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 05:14:23 +0000

    change to arrow functions takeUntil test

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,13 +1,13 @@
 const assert = require('chai').assert;
 const takeUntil = require('../takeUntil');
 
-describe("takeUntil function", function() {
-  it("returns [1, 2, 5, 7, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5]", function() {
+describe("takeUntil function", () => {
+  it("returns [1, 2, 5, 7, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5]", () => {
     const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
     const results1 = takeUntil(data1, x => x < 0);
     assert.deepEqual(results1, [1, 2, 5, 7, 2]);
   });
-  it("returns ['I've','been','to','Hollywood'] for ['I've', 'been', 'to', 'Hollywood', ',', 'I've', 'been', 'to', 'Redwood']", function() {
+  it("returns ['I've','been','to','Hollywood'] for ['I've', 'been', 'to', 'Hollywood', ',', 'I've', 'been', 'to', 'Redwood']", () => {
     const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
     const results2 = takeUntil(data2, x => x === ',');
     assert.deepEqual(results2, ["I've","been","to","Hollywood"]);
