95647f400d0f8c18593abcf01c72af02d3924b3f
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:40:30 +0000

    refactor head test mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,18 +1,8 @@
-/** TESTING MANUALLY
- *
- * // test assertEqualTest.js
- * const assertEqual = require('../assertEqual');
- * // test headTest.js
- * const head = require('../head');
- *
- * // TEST CODE
- * assertEqual(head([5,6,7]), 5); // passed!
- * assertEqual(head(["Hello", "Lighthosue", "Labs"]), "Hello");  // passed!
- *
- */
-
+//import assert library from chai package.
 const assert = require('chai').assert;
-const head   = require('../head');
+
+// import head function.
+const head = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
