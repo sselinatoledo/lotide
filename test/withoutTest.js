09f5329a8dd37401485efabe7d663233dec8c084
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 05:14:48 +0000

    change to arrow functions without test

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,16 +1,16 @@
 const assert = require('chai').assert;
 const without = require('../without');
 
-describe("without function", function() {
-  it("returns 2,3 === 2,3 for without([1, 2, 3], [1])", function() {
+describe("without function", () => {
+  it("returns 2,3 === 2,3 for without([1, 2, 3], [1])", () => {
     without([1, 2, 3], [1]);
     assert.deepEqual([2, 3], [2, 3]);
   });
-  it("returns 1,2 === 1,2 for without(['1', '2', '3'], [1, 2, '3'])", function() {
+  it("returns 1,2 === 1,2 for without(['1', '2', '3'], [1, 2, '3'])", () => {
     without(["1", "2", "3"], [1, 2, "3"]);
     assert.deepEqual([1, 2], [1, 2]);
   });
-  it("returns hello,world,lighthouse === hello,world,lighthouse for without(['hello', 'world', 'lighthouse'])", function() {
+  it("returns hello,world,lighthouse === hello,world,lighthouse for without(['hello', 'world', 'lighthouse'])", () => {
     const words = ["hello", "world", "lighthouse"];
     without(words, ["lighthouse"]);
     assert.deepEqual(words, ["hello", "world", "lighthouse"]);
