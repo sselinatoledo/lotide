1d3f1f768993a0c2b3b55b5ad02957cafe0b3985
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 18:34:49 +0000

    remove assertEqual minor comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,4 @@
-// function that takes in 2 values and returns false if they are not equal and true otherwise.
 const assertEqual = function(actual, expected) {
-  
-  // conditional statement that tells if 2 values are equal or not.
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -9,6 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// export function assertEqual.
 module.exports = assertEqual;
 
