42289469262b16140afba68151304097728786e0
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 21:00:38 +0000

    remove takeUntil minor comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,13 +1,6 @@
-// function that takes in an array and callback and returns a slice of the array based on the criteria specified in the callback.
 const takeUntil = function(array, callback) {
-
-  // create an empty array to store the result.
   const result = [];
-
-  // loop through for each element of the array.
   for (let element of array) {
-
-    // if the callback is true return result.
     if (callback(element)) {
       return result;
     }
@@ -15,5 +8,4 @@ const takeUntil = function(array, callback) {
   }
 };
 
-// export takeUntil function.
 module.exports = takeUntil;
