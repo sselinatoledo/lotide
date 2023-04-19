779a752b6f659f9bf8931d07f5cfa2efc78de9a9
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 20:52:17 +0000

    remove assertObjectsEqual minor comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,13 +1,7 @@
-// import function eqObjects.
 const eqObjects = require('./eqObjects');
 
-// function that takes in 2 objects and returns true if they are equal and false otherwise.
 const assertObjectsEqual = function(actual, expected) {
-
-  // import library.
   const inspect = require("util").inspect;
-
-  // conditional statement that tells if eqObjects are equal or not.
   if (eqObjects(actual, expected)) {
     console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
@@ -15,5 +9,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// export function assertObjectsEqual.
 module.exports = assertObjectsEqual;
