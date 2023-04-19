326f7237dcdf868076d64dee0101c691bf32becd
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:12:07 +0000

    check code for errors

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,24 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  
+// function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
+const eqArrays = function(arr1, arr2) {
+
   // check the length of the arrays if it is the same.
-  if (array1.length !== array2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
   // loop through the array so you can check the inside of it.
-  for (let i = 0; i < array1.length; i++) {
-    
+  for (let i = 0; i < arr1.length; i++) {
+
     // check the nested arrays if it is the same.
-    const value1 = array1[i]
-    const value2 = array2[i]
-    if (Array.isArray(value1) && Array.isArray(value2)){
+    const value1 = arr1[i];
+    const value2 = arr2[i];
+    if (Array.isArray(value1) && Array.isArray(value2)) {
       if (!eqArrays(value1, value2)) {
         return false;
       }
-    }
 
-    // make this as else if and check the value inside the array if it is the same.
-    else if (value1 !== value2) {
+      // make this as else if and check the value inside the array if it is the same.
+    } else if (value1 !== value2) {
       return false;
     }
   }
@@ -27,4 +26,6 @@ const eqArrays = function(array1, array2) {
   // Happy path.
   return true;
 };
+
+// export function eqArrays.
 module.exports = eqArrays;
