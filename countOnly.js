82136c5e4c2a1899845c575346f1dd5973f93139
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:11:20 +0000

    clean up code

--- countOnly.js
+++ countOnly.js
@@ -1,41 +1,29 @@
-// this function should take in a collection of items => array
-// return counts for a specific subset of those items => object
-
-// it won't count everything
-// In order to decide what to count, it will be given an idea of which items we care about and it will only count those, ignoring the others => conditional
-
-// array + object = return an object that is truthy value
-// false in the object and not included in the array is not counted
-
-// array = allItems
-// object = itemsToCount
-
-// 1. cannot read property => make a new empty object and return it
-// equals to undefined
-
-// 2. for loop
-// to count them all
-
-// 3. if statement
-// set the empty object and index of the array to 1 and increment it to many times it appears
-
-// 4. otherwise
-// set it = to 1 only
-
-// 5. need to add another if because of the false from the object itemsToCount
-
-// SOLUTION
+// function that takes in a collection of items and returns a specific subset of those items.
 const countOnly = function(allItems, itemsToCount) {
+
+  // store the values of the object result.
   const result = {};
-  for (let item of allItems) {
+
+  // loop through the allItems to check how many times eaxh item appear.
+  for (const item of allItems) {
+
+    // if the value is found in the itemsToCount object.
     if (itemsToCount[item]) {
+      
+      // then we need to increment the count of each item into result as we encounter it.
       if (result[item]) {
         result[item] += 1;
+        
+        // Otherwise, set it only to 1.
       } else {
         result[item] = 1;
       }
     }
   }
+
+  // return the result object.
   return result;
 };
+
+// export function countOnly.
 module.exports = countOnly;
