89cd8fb38fed47e618b8f36dbe8747444a2a99f8
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 05:13:51 +0000

    change to arrow functions map test

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,15 +1,15 @@
 const assert = require('chai').assert;
 const map = require('../map');
 
-describe("map function", function() {
-  it("returns g,c,t,m,t for ['ground', 'control', 'to', 'major', 'tom']", function() {
+describe("map function", () => {
+  it("returns g,c,t,m,t for ['ground', 'control', 'to', 'major', 'tom']", () => {
     const words = ["ground", "control", "to", "major", "tom"];
     const results1 = map(words, function(word) {
       return word[0];
     });
     assert.deepEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
   });
-  it("returns ground,control,to,major,tom for ['ground', 'control', 'to', 'major', 'tom']", function() {
+  it("returns ground,control,to,major,tom for ['ground', 'control', 'to', 'major', 'tom']", () => {
     const words = ["ground", "control", "to", "major", "tom"];
     assert.deepEqual(words, ["ground", "control", "to", "major", "tom"]);
   });
