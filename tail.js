478a569f15bec007ed1d5a402b6116a5588bbe7f
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:21:24 +0000

    cleaning up my comments

--- tail.js
+++ tail.js
@@ -1,5 +1,9 @@
-// FUNCTION IMPLEMENTATION
+// function that takes in an array and returns everything except the first element of the array.
 const tail = function(actual) {
+
+  // using slice method to remove the first index of the array.
   return actual.slice(1);
 };
+
+// export tail function.
 module.exports = tail;
