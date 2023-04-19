25997b5c33addf51a3f4ba59d5dfed0521d82e6d
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:35:34 +0000

    refactor and testing with mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,49 +1,23 @@
-/* MANUAL TESTING
-// test assertEqual.js
-const assertEqual = require('../assertEqual');
-
-// test eqArrays.js
-const eqArrays = require('../eqArrays');
-
-// TEST CODE 1
-console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false, add another tester //
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true //
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false //
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true //
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false //
-
-// TEST CODE 2
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS //
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => should FAIL //
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS //
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL //
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS //
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL //
-
-// TEST CODE 3 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
-console.log(eqArrays([[2, 3], [4]], [2, 3], [4, 5])) // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
-*/
-
-// MOCHA AND CHAI TESTING
+//import assert library from chai package.
 const assert = require('chai').assert;
+
+// import eqArrays function.
 const eqArrays = require('../eqArrays');
 
-describe("eqArrays function", function() {
-  it("returns true === true for ([1, 2, 3],[1, 2, 3]), true)", function() {
+describe("eqArrays function", () => {
+  it("returns true for [1, 2, 3] === [1, 2, 3]", () => {
     assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
-  it("returns true === true for (['1', '2', '3'],['1', '2', '3']), true)", function() {
+  it("returns true for ['1', '2', '3'] === ['1', '2', '3']", () => {
     assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
   });
-  it("returns false !== true for ([1, 2, 3],[1, 2, 3, 4]), true)", function() {
+  it("returns false for [1, 2, 3] === [1, 2, 3, 4]", () => {
     assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
   });
-  it("returns false !== true for ([1, 2, 3],[3, 2, 1), true)", function() {
+  it("returns false for [1, 2, 3] === [3, 2, 1]", () => {
     assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
   });
-  it("returns false !== true for (['1', '2', '3'],['1', '2', 3]), true)", function() {
+  it("returns false for ['1', '2', '3'] === ['1', '2', 3]", () => {
     assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
   });
 });
