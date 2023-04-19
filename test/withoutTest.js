c4cbd7e8a9888675218c85a7e67731c448a559da
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:44:01 +0000

    refactor without test mocha and chai

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,21 +1,3 @@
-// require without and assertArraysEqual functions to run the test code in here.
-// const without = require('../without');
-// const assertArraysEqual = require('../assertArraysEqual');
-
-// // Test Code 1
-// without([1, 2, 3], [1]); // => [2, 3]
-// assertArraysEqual([2, 3], [2, 3]); // => pass!
-
-// // Test Code 2
-// without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-// assertArraysEqual([1, 2], [1, 2]); // => pass!
-
-// // Test Code 3
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]); // Make sure the original array was not altered by the without function => pass!
-
-// // mocha and chai testing
 const assert = require('chai').assert;
 const without = require('../without');
 
