3a5b06c7198faf2f1beece374409c3482418154c
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:16:00 +0000

    check for errors

--- head.js
+++ head.js
@@ -1,5 +1,7 @@
-// FUNCTION IMPLEMENTATION
+// function that takes in an array and returns the first element in the array.
 const head = function(actual) {
   return actual[0];
 };
+
+// export head function.
 module.exports = head;
