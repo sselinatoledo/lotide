771906f97767e44122b474cf2ff862d6139e3eb6
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 05:13:31 +0000

    change to arrow functions letterPositions test

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
 const letterPositions = require('../letterPositions');
 
-describe("letterPositions function", function() {
-  it("returns [3,5,15,18] === [3,5,15,18] for letter h in 'lighthouse in the house'", function() {
+describe("letterPositions function", () => {
+  it("returns [3,5,15,18] === [3,5,15,18] for letter h in 'lighthouse in the house'", () => {
     assert.deepEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
   });
-  it("returns [0] === [0] for letter l in 'lighthouse in the house' ", function() {
+  it("returns [0] === [0] for letter l in 'lighthouse in the house' ", () => {
     assert.deepEqual(letterPositions("lighthouse in the house").l, [0]);
   });
 });
