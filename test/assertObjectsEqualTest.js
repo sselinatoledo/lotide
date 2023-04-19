08c970493f27712d894297af480825ea47572ec8
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:51:14 +0000

    checking assertObjectsEqual test codes

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,6 +1,6 @@
 // import assertObjectsEqual function
 const assertObjectsEqual = require('../assertObjectsEqual');
 
-// TEST CODE
+// TEST CODE 
 assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // passed!
 assertObjectsEqual({ a: '1', b: 2 }, { a: '1', b: 2, c: '3' }); // failed!
