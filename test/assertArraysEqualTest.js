326a157e1a209ad8b45fb2bdf683c2c7c9315eef
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 04:49:53 +0000

    checking assertArraysEqual test codes

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,7 @@
 // import assertArraysEqual function.
 const assertArraysEqual = require('../assertArraysEqual');
 
-// TEST CODE
+// TEST CODE 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);   // passed!
 assertArraysEqual([1, 2, 3], [3, 2, 1]);   // failed!
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);   // passed!
