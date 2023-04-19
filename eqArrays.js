a8d3bbb4500989bcce05c37bded253077bb83e8d
sselinatoledo <sselinatoledo@gmail.com>
2023-04-19 20:53:41 +0000

    remove eqArrays minor comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,19 @@
-// function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
 const eqArrays = function(arr1, arr2) {
-
-  // check the length of the arrays if it is the same.
   if (arr1.length !== arr2.length) {
     return false;
   }
-
-  // loop through the array so you can check the inside of it.
   for (let i = 0; i < arr1.length; i++) {
-
-    // check the nested arrays if it is the same.
     const value1 = arr1[i];
     const value2 = arr2[i];
     if (Array.isArray(value1) && Array.isArray(value2)) {
       if (!eqArrays(value1, value2)) {
         return false;
       }
-
-      // make this as else if and check the value inside the array if it is the same.
     } else if (value1 !== value2) {
       return false;
     }
   }
-
-  // return true, happy path.
   return true;
 };
 
-// export function eqArrays.
 module.exports = eqArrays;
