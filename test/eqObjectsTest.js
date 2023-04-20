2ec2c85452f022b9ebdd12514c78f94c95338ba3
sselinatoledo <sselinatoledo@gmail.com>
2023-04-20 03:01:25 +0000

    changing description in the it block eqObjects test

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -6,13 +6,13 @@ const eqObjects = require('../eqObjects');
 
 // TEST CODE
 describe("eqObjects function", () => {
-  it("returns true === true for the same primitive values", () => {
+  it("returns true for objects with same properties", () => {
     assert.deepEqual(eqObjects({color: 'red', size: 'medium'}, {size: 'medium', color: 'red'}), true);
   });
-  it("returns true === true for the same array values", () => {
+  it("returns true for objects with same array values", () => {
     assert.deepEqual(eqObjects({colors: ['red', 'blue'], size: 'medium'}, { size: 'medium', colors: ['red', 'blue']}), true);
   });
-  it("returns true === true for the same object values", () => {
+  it("returns true for objects with same nested object values", () => {
     assert.deepEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
   });
 });
