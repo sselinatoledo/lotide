a1757f0f12af7bcc62c900703ab9124c9cae9a12
sselinatoledo <sselinatoledo@gmail.com>
2023-04-20 03:05:25 +0000

    changing it block takeUntil test

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -2,14 +2,9 @@ const assert = require('chai').assert;
 const takeUntil = require('../takeUntil');
 
 describe("takeUntil function", () => {
-  it("returns [1, 2, 5, 7, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5]", () => {
+  it("returns the array up until the function callback is true", () => {
     const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
     const results1 = takeUntil(data1, x => x < 0);
     assert.deepEqual(results1, [1, 2, 5, 7, 2]);
-  });
-  it("returns ['I've','been','to','Hollywood'] for ['I've', 'been', 'to', 'Hollywood', ',', 'I've', 'been', 'to', 'Redwood']", () => {
-    const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-    const results2 = takeUntil(data2, x => x === ',');
-    assert.deepEqual(results2, ["I've","been","to","Hollywood"]);
   });
 });
