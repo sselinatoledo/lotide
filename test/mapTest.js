136830fad7597fa436e424d083b8770fea169a87
sselinatoledo <sselinatoledo@gmail.com>
2023-04-20 03:04:03 +0000

    changing it block map test

--- test/mapTest.js
+++ test/mapTest.js
@@ -2,15 +2,11 @@ const assert = require('chai').assert;
 const map = require('../map');
 
 describe("map function", () => {
-  it("returns g,c,t,m,t for ['ground', 'control', 'to', 'major', 'tom']", () => {
+  it("returns a new array with the first letter of each word in the array", () => {
     const words = ["ground", "control", "to", "major", "tom"];
     const results1 = map(words, function(word) {
       return word[0];
     });
     assert.deepEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
-  });
-  it("returns ground,control,to,major,tom for ['ground', 'control', 'to', 'major', 'tom']", () => {
-    const words = ["ground", "control", "to", "major", "tom"];
-    assert.deepEqual(words, ["ground", "control", "to", "major", "tom"]);
   });
 });
