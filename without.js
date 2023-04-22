659a8f76a4e1c4219963b90072f242b6ffe1b564
sselinatoledo <sselinatoledo@gmail.com>
2023-04-22 01:46:32 +0000

    update code for this has an error

--- without.js
+++ without.js
@@ -1,7 +1,7 @@
 const without = function(source, itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
-    if (source[i] !== itemsToRemove[i]) {
+    if (!itemsToRemove.includes(source[i])) {
       newArray.push(source[i]);
     }
   }
